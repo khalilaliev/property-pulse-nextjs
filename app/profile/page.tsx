ca91b066fc527fb44ac189082a68354cdf966ced
khalilaliev <khalilaliev0@gmail.com>
2024-10-02 18:58:31 +0200

    add user profile property listing

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,14 +4,24 @@ import { ISessionUser, getSessionUser } from "@/utils/getSessionUser";
 import Image from "next/image";
 import { FC } from "react";
 import profileDefault from "@/assets/images/profile.png";
+import ProfileProperties from "@/components/ProfileProperties";
+import { IProperty } from "@/interfaces";
 
-const ProfilePage: FC = async (): Promise<any> => {
+const ProfilePage: FC = async (): Promise<JSX.Element> => {
   await connectDb();
   const sessionUser = await getSessionUser();
   const { userId } = sessionUser as ISessionUser;
   if (!userId) {
     throw new Error("User ID is required");
   }
+  const properties = (await Property.find({
+    owner: userId,
+  }).lean()) as IProperty[];
+  const transformedProperties = properties.map((property) => ({
+    ...property,
+    _id: property._id.toString(),
+    owner: property.owner.toString(),
+  }));
 
   return (
     <section className="bg-blue-50">
@@ -42,60 +52,8 @@ const ProfilePage: FC = async (): Promise<any> => {
 
             <div className="md:w-3/4 md:pl-4">
               <h2 className="text-xl font-semibold mb-4">Your Listings</h2>
-              <div className="mb-10">
-                <a href="/property.html">
-                  <img
-                    className="h-32 w-full rounded-md object-cover"
-                    src="/images/properties/a1.jpg"
-                    alt="Property 1"
-                  />
-                </a>
-                <div className="mt-2">
-                  <p className="text-lg font-semibold">Property Title 1</p>
-                  <p className="text-gray-600">Address: 123 Main St</p>
-                </div>
-                <div className="mt-2">
-                  <a
-                    href="/add-property.html"
-                    className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
-                  >
-                    Edit
-                  </a>
-                  <button
-                    className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
-                    type="button"
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
-              <div className="mb-10">
-                <a href="/property.html">
-                  <img
-                    className="h-32 w-full rounded-md object-cover"
-                    src="/images/properties/b1.jpg"
-                    alt="Property 2"
-                  />
-                </a>
-                <div className="mt-2">
-                  <p className="text-lg font-semibold">Property Title 2</p>
-                  <p className="text-gray-600">Address: 456 Elm St</p>
-                </div>
-                <div className="mt-2">
-                  <a
-                    href="/add-property.html"
-                    className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
-                  >
-                    Edit
-                  </a>
-                  <button
-                    className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
-                    type="button"
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
+
+              <ProfileProperties properties={transformedProperties} />
             </div>
           </div>
         </div>
---
+++ components/ProfileProperties.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { IProperty } from "@/interfaces";
+import Image from "next/image";
+import { FC, useState } from "react";
+
+interface IProfilePropertyProp {
+  properties: IProperty[];
+}
+
+const ProfileProperties: FC<IProfilePropertyProp> = ({
+  properties: initialProperty,
+}) => {
+  const [properties, setProperties] = useState(initialProperty);
+  return properties.map((property) => (
+    <div className="mb-10" key={property._id}>
+      <a href="/property.html">
+        <Image
+          className="h-32 w-full rounded-md object-cover"
+          src={property.images[0]}
+          alt="Property 1"
+          width={1000}
+          height={200}
+        />
+      </a>
+      <div className="mt-2">
+        <p className="text-lg font-semibold">{property.name}</p>
+        <p className="text-gray-600">
+          Address: {property.location.street} {property.location.city}{" "}
+          {property.location.state}
+        </p>
+      </div>
+      <div className="mt-2">
+        <a
+          href="/add-property.html"
+          className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
+        >
+          Edit
+        </a>
+        <button
+          className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
+          type="button"
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  ));
+};
+
+export default ProfileProperties;
