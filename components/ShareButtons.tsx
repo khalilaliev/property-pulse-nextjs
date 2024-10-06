5231475567a40b79daa5ab20808cbbfe57ca09d1
khalilaliev <khalilaliev0@gmail.com>
2024-10-06 09:21:43 +0200

    add property share buttons

--- components/ShareButtons.tsx
+++ components/ShareButtons.tsx
@@ -1,12 +1,85 @@
+"use client";
 import { IPropertyProp } from "@/interfaces";
 import { FC } from "react";
-import { FaShare } from "react-icons/fa";
+import {
+  EmailShareButton,
+  FacebookShareButton,
+  TelegramShareButton,
+  ViberShareButton,
+  WhatsappShareButton,
+  FacebookIcon,
+  TelegramIcon,
+  ViberIcon,
+  WhatsappIcon,
+  EmailIcon,
+  VKShareButton,
+  TwitterShareButton,
+  TwitterIcon,
+  VKIcon,
+} from "react-share";
 
 const ShareButtons: FC<IPropertyProp> = ({ property }) => {
+  const shareUrl: string = `${process.env.NEXT_PUBLIC_DOMAIN}/properties/${property._id}`;
   return (
-    <button className="bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
-      <FaShare className="fas fa-share mr-2"></FaShare> Share Property
-    </button>
+    <>
+      <h3 className="text-xl font-bold text-center pt-2">
+        {" "}
+        Share this property
+      </h3>
+      <div className="flex justify-center gap-3 pb-5">
+        <FacebookShareButton
+          className="hover:scale-110 transition-all duration-300"
+          url={shareUrl}
+          //@ts-ignore
+          quote={`${property.name}`}
+          hashtag={`#${property.type.replace(/\s/g, "")}ForRent`}
+        >
+          <FacebookIcon size={40} round />
+        </FacebookShareButton>
+        <TelegramShareButton
+          className="hover:scale-110 transition-all duration-300"
+          url={shareUrl}
+          title={`${property.name}`}
+        >
+          <TelegramIcon size={40} round />
+        </TelegramShareButton>
+        <ViberShareButton
+          className="hover:scale-110 transition-all duration-300"
+          url={shareUrl}
+          title={`${property.name}`}
+        >
+          <ViberIcon size={40} round />
+        </ViberShareButton>
+        <WhatsappShareButton
+          className="hover:scale-110 transition-all duration-300"
+          url={shareUrl}
+          title={`${property.name}`}
+        >
+          <WhatsappIcon size={40} round />
+        </WhatsappShareButton>
+        <EmailShareButton
+          className="hover:scale-110 transition-all duration-300"
+          url={shareUrl}
+          subject={`${property.name}`}
+        >
+          <EmailIcon size={40} round />
+        </EmailShareButton>
+        <VKShareButton
+          className="hover:scale-110 transition-all duration-300"
+          url={shareUrl}
+          title={`${property.name}`}
+        >
+          <VKIcon size={40} round />
+        </VKShareButton>
+        <TwitterShareButton
+          className="hover:scale-110 transition-all duration-300"
+          url={shareUrl}
+          title={property.name}
+        >
+          <TwitterIcon size={40} round />
+        </TwitterShareButton>
+      </div>
+    </>
   );
 };
 
